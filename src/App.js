1d8ba1498d2c8b169edd125bf6262e4aa3513642
sp004 <souvikpal4049@gmail.com>
2022-07-10 04:13:29 +0530

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import LaptopDetails from './pages/LaptopDetails';
+
 import Home from './pages/Home';
 import classes from './App.module.css'
 import LaptopContextProvider from './context/laptop-context';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Header/Navbar';
-import NotFound from './pages/NotFound';
+import { lazy, Suspense } from 'react';
+
+const LaptopDetails = lazy(() => import('./pages/LaptopDetails'))
+const NotFound = lazy(() => import('./pages/NotFound'))
 
 function App() {
   return (
@@ -13,11 +16,13 @@ function App() {
       <div className={classes.app}>
         <Navbar />
         <div className={classes.container}>
+          <Suspense fallback={<p>Loading...</p>}>
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/laptop/:model' element={<LaptopDetails />} />
               <Route path='*' element={<NotFound />} />
             </Routes>
+          </Suspense>
         </div>
         <Footer />
       </div>
